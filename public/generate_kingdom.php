7ed42bc1366cd2e40a8dd5bf260f6182b17b76b9
jthayne <webadmin@thaynefam.org>
2024-08-05 14:39:08 -0500

    Dummy change to trigger push

--- public/generate_kingdom.php
+++ public/generate_kingdom.php
@@ -6,6 +6,7 @@ use Dominion\Cards\Cards;
 use Dominion\Kingdom\Kingdom;
 
 require_once __DIR__ . '/../bootstrap/init.php';
+
 $db = require_once __DIR__ . '/../bootstrap/db.php';
 
 $cards = new Cards($db);
